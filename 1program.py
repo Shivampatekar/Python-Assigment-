1ecd4c2ec5b439f54ce245882ae04ad0f93eae65
Shivampatekar <shivampatekar46@gmail.com>
2026-06-03 07:49:48 -0400

    Added 1program.py

---
+++ 1program.py
@@ -0,0 +1,159 @@
+
+
+
+
+
+
+print("hello core2web")
+
+x=10
+print(x)
+
+x=10.1
+print(x)
+
+x=2+3j
+print(x)
+
+my_name="shivam"
+print(my_name)
+
+#boolean value 
+
+x=True
+y=False
+print(x)
+print(y)
+
+#show type 
+
+num=25
+print(type(num))
+
+num1=31.5
+print(type(num1))
+
+# sum of 3 number
+
+x=10
+y=20
+z=10.5
+
+w=x+y+z
+print(w)
+
+# create list 
+
+my_list=[10,20,30,"shivam","prem"]
+print(my_list)
+
+# assessing list by using index 
+
+print(my_list[0])         
+print(my_list[1])
+print(my_list[2])
+print(my_list[3])
+print(my_list[4])
+
+# create tuple
+
+my_tup=(10,"shiva",20)
+print(my_tup)
+
+# sum of all list index 
+
+x=[10,20,30]
+list_sum=(x[0]+x[1]+x[2])
+print(list_sum)
+
+
+# create set 
+
+my_set={10,11,10,20,11}
+print(my_set)
+
+# 14. create list and perform list operation 
+
+my_list[3]="core2web"
+print(my_list)
+
+#15. create dictionary 
+student={
+        'name':"shivam",
+        'age':19,
+        'mark':85
+        }
+print(student) 
+
+print(student['name'])
+print(student['age'])
+print(student['mark'])
+
+
+# 16. asiigen new value to key 
+
+new_dict={
+        "name":"prem",
+        "age":25,
+        "hoby":"play cricket"
+        }
+print(new_dict)
+
+new_dict["age"]=26
+print(new_dict)
+
+#17. check existing key in dictionary
+dictdata={
+        0:10,
+        1:20,
+        2:25,
+        3:30,
+        4:40
+        }
+sum_dict=dictdata[0]+dictdata[3]
+print(sum_dict)
+
+#18. assign value None
+
+x=None
+print(x)
+
+#19. three sting display 
+
+str1='shivam'
+
+str2="shivam"
+
+str3='''shivam'''
+
+print(str1)
+print(str2)
+print(str3)
+
+
+#20. assign eacj value 
+
+intval=10
+flval=10.5
+comval=2+3j 
+strval="hello sir"
+my_list=[10,"name","core2web",19,11]
+my_tup=(10,"shivam",22.3)
+my_dict={"name":"ALPHA","age":20}
+my_set={10,11,12,10,11,12}
+boolean=True
+none=None
+
+print(intval)
+print(flval)
+print(comval)
+print(strval)
+print(my_list)
+print(my_tup)
+print(my_dict)
+print(my_set)
+print(boolean)
+print(none)
+
+
+
