fb0174ad8846c498347007ab7b31353c00a6aac5
Shivampatekar <shivampatekar46@gmail.com>
2026-06-11 11:11:07 -0400

    new file add

--- 2program.py
+++ 2program.py
@@ -1,5 +1,165 @@
+# create 2 variable
+
+x=15
+y=4
+
+# All arithmatic operation 
+
+add=x+y
+sub=x-y
+mul=x*y
+div=x/y
+mod=x%y
+fdiv=x//y
+exp=x**y
+
+print(add)
+print(sub)
+print(mul)
+print(div)
+print(mod)
+print(fdiv)
+print(exp)
+
+print(".......................................")
+
+# 2 create variable num=50 Assigment operator 
+
+num=50 
+print(num)
+num+=10
+print(num)
+num-=5
+print(num)
+num*=2
+print(num)
+num/=4
+print(num)
+num%=5
+print(num)
+num //=3
+print(num)
+
+print("................................")
+
+# 3 create 2 variable a=25 and b=30 Relational operator 
+
+a=25 
+b=30 
+
+print(a<b)
+print(a>b)
+print(a<=b)
+print(a>=b)
+print(a==b)
+print(a!=b)
+
+print(".................................")
+
+# 4. create 3 variable x=10 ,y=20 z=30 logical operator
+
+x=10
+y=20 
+z=30
+
+print(x<y and y<z)
+print(x>y or y<z)
+print(x!=y)
+
+print("........................................")
+
+# 5.  2 list and 2 integer 
+
+a=100
+b=100
+
+print(id(a))
+print(id(b))
+
+print(a is b )
+print(a is not b )
+
+lis1=[1,2,3]
+lis2=[1,2,3]
+
+print(id(lis1))
+print(id(lis2))
+
+print(lis1 is lis2)
+print(lis1 is not lis2)
+print(lis1 == lis2)
+
+print('..................................')
+
+# 6. create llist colur 
+
+color=["red","blue","green","yellow"]
+
+if ("blue" in color):
+    print("blue exit")
+else:
+    print("not")
+
+print("purple" in color)
+print("pink" not in color) 
+print("red" not in color)
+
+print("....................................")
+
+
+# 7. bitwise operator 
+
+x=12
+y=10
+
+print(x&y)
+print(x|y)
+print(x^y)
+print(~x)
+print(x<<2)
+print(x>>2)
+
+print(".....................................")
+
+# 8 type function used 
+
+intval=10
+print(type(intval))
+
+flo=5.5
+print(type(flo))
+
+st="shivam"
+print(type(st))
+
+print("...................................")
+
+# 9. concatenate 
+
+first="shivam"
+second="patekar"
+
+print(first+second)
+print(first*3)
+print(second*2)
+print((first+" ")*2 + second )
+
+print("...............................")
+
+# 10. 
+a=10
+b=5
+c=3
+
+print(a+b*c)
+print((a+b)*c)
+print(a**b//c)
+print(a<b and b>c or a==10)
 
 
 
-print(1)
+
+
+
+
 
