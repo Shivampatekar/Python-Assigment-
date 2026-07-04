3097f344f46691952030d35dad38cdbcfe2f9ed5
Shivampatekar <shivampatekar46@gmail.com>
2026-07-04 14:14:29 -0400

    Added program 4

---
+++ 4program.py
@@ -0,0 +1,57 @@
+
+# interger input for user 
+x=int(input("Enter number :"))
+print("you entered number",x)
+
+# take string as input 
+
+x=str(input("Enter string :"))
+print("you entered string",x)
+
+# float as input 
+
+x=float(input("Enter number :"))
+print("you entered float",x)
+
+# 2 input interger and their sum 
+
+
+x=int(input("Enter number :"))
+y=int(input("Enter number :"))
+print("sum of 5 and 10 is :",x+y)
+
+
+# user name string and age integer
+
+x=str(input("name :"))
+y=int(input("age :"))
+
+:print("name",x)
+print("age",y)
+
+# 6. average of float number 
+
+x=float(input("Enter number1 :"))
+y=float(input("Enter number1 :"))
+z=float(input("Enter number1 :"))
+average=(x+y+z)/3
+print("average of number :",average)
+
+# 7. print input type 
+
+x=int(input("Enter number1 :"))
+print(type(x))
+
+x=input("Enter number1 :")
+print(type(x))
+
+x=float(input("Enter number1 :"))
+print(type(x))
+
+# multiplication 
+
+x=int(input("Enter number :"))
+y=int(input("Enter number :"))
+mul=x*y
+print(mul)
+
