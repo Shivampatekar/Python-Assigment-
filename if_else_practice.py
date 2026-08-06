0c004c417cf5530c24895c8273828ada50c67f32
Shivampatekar <shivampatekar46@gmail.com>
2026-08-06 14:43:18 -0400

    Added program if else practice

---
+++ if_else_practice.py
@@ -0,0 +1,105 @@
+# p1.py
+
+x=10
+y=20
+print(x+y)
+
+#p2.py
+
+x = int(input("enter num: "))
+if(x>0):
+    print(x)
+
+#p3.py
+
+x= int(input("Enter "))
+if x>0:
+    print(x)
+    if(x>0):
+        print(x)
+
+# p4.py
+
+x = int(input("Enter value"))
+if x > 0:
+    print(x)
+else:
+    print(x)
+
+#p5.py
+
+
+x = int(input("Enter value "))
+if x > 0:
+    print(x)
+else:
+    print(x)
+
+#p6.py
+
+x = int(input("Enter value"))
+if x > 0:
+    print(x)
+else:
+    print(x)
+
+#p7.py
+
+x = int(input("Enter value"))
+if x > 0:
+    print(x)
+if x == 0:
+    print(x)
+if x < 0:
+    print(x)
+
+# p8.py
+
+x = int(input("Enter value"))
+if x > 0:
+    print(x)
+elif x == 0:
+    print(x)
+else:
+    print(x)
+
+# p9.py
+
+x = int(input("Enter value"))
+if x > 0:
+    print(x)
+elif x == 0:
+    print(x)
+else:
+    print(x)
+
+
+# p10.py
+
+num1 = 10
+num2 = 20
+sumData = num1 + num2
+subData = num2 - num1
+
+if num1 > num2:
+    print(sumData)
+else:
+    print(subData)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
